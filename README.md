32c216ce5ff107115b8cb229d1b697911d04ed75
Maria Chikhireva <mchikhireva@gmail.com>
2024-09-05 17:48:56 +0300

    Add generate_diff module, update .gitignore and record asciinema

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/MariaChikh/python-project-50/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/MariaChikh/python-project-50/actions)
+
+How it works:
+
+[![asciicast](https://asciinema.org/a/YYcms1mAnpB120WXLK1K5tuhQ.svg)](https://asciinema.org/a/YYcms1mAnpB120WXLK1K5tuhQ)
---
+++ gendiff/generate_diff.py
@@ -0,0 +1,27 @@
+import json
+
+
+def generate_diff(file_path1, file_path2):
+    file1 = json.load(open(file_path1))
+    file2 = json.load(open(file_path2))
+    union_keys = set(file1.keys()).union(file2.keys())
+
+    diff = {}
+    for key in union_keys:
+
+        if key in file1 and key not in file2:
+            diff[key] = f"- {key}: {file1[key]}"
+
+        elif key not in file1 and key in file2:
+            diff[key] = f"+ {key}: {file2[key]}"
+
+        elif file1[key] != file2[key]:
+            diff[key] = f"- {key}: {file1[key]}\n+ {key}: {file2[key]}"
+
+        else:
+            diff[key] = f"  {key}: {file1[key]}"
+
+    sorted_diff = dict(sorted(diff.items()))
+    result = '{\n' +'\n'.join(sorted_diff.values())+'\n}'
+    return result
+
