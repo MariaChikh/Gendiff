f70a43db721829415e56e0e883b3bb29fbc5feee
Maria Chikhireva <mchikhireva@gmail.com>
2024-09-10 20:49:18 +0300

    update tests

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -10,18 +10,18 @@ def generate_diff(file_path1, file_path2):
     for key in union_keys:
 
         if key in file1 and key not in file2:
-            diff[key] = f"- {key}: {file1[key]}"
+            diff[key] = f"  - {key}: {file1[key]}"
 
         elif key not in file1 and key in file2:
-            diff[key] = f"+ {key}: {file2[key]}"
+            diff[key] = f"  + {key}: {file2[key]}"
 
         elif file1[key] != file2[key]:
-            diff[key] = f"- {key}: {file1[key]}\n+ {key}: {file2[key]}"
+            diff[key] = f"  - {key}: {file1[key]}\n  + {key}: {file2[key]}"
 
         else:
-            diff[key] = f"  {key}: {file1[key]}"
+            diff[key] = f"    {key}: {file1[key]}"
 
     sorted_diff = dict(sorted(diff.items()))
-    result = '{\n' +'\n'.join(sorted_diff.values())+'\n}'
+    result = '{\n' +'\n'.join(sorted_diff.values())+'\n}\n'
     return result.lower()
 
