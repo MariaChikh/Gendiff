2e68a7b7cdab13ad987f6fbe04c4fdece3196dcf
Maria Chikhireva <mchikhireva@gmail.com>
2024-09-11 22:14:27 +0300

    fix tests errors at step 6

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,10 +3,10 @@ from gendiff.generate_diff import generate_diff
 
 
 @pytest.mark.parametrize('file1, file2, expected', [
-    ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/expected.txt')
-    ('tests/fixtures/file1.yml', 'tests/fixtures/file2.yml', 'tests/fixtures/expected.txt')
+    ('tests/fixtures/file1.json', 'tests/fixtures/file2.json', 'tests/fixtures/expected.txt'),
+    ('tests/fixtures/file1.yaml', 'tests/fixtures/file2.yaml', 'tests/fixtures/expected.txt')
     ]
-    )
+)
 
 def test_gendiff(file1, file2, expected):
     diff = generate_diff(file1, file2)
